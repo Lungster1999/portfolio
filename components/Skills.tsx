c659791aa6c1971a49c5b5104e6c98971e46cbc4
Lungile <lungile.matsinhe@gmail.com>
2023-06-17 19:38:33 +0200

    my first project commit

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import React from 'react';
 import Skill from './Skill';
 import { motion } from "framer-motion";
--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Set the assetPrefix to the repository name
+    // Replace 'repository-name' with your actual repository name
+    // Make sure to include the trailing slash '/'
+    assetPrefix: '/portfolio/',
+  
+    // Specify basePath if your application is not hosted at the root of the domain
+    // For example, if your GitHub Pages URL is https://username.github.io/my-app/
+    // Set basePath to '/my-app'
+  
+    // Configure the webpack publicPath to match the assetPrefix
+    // This is required for correct asset loading on GitHub Pages
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.output.publicPath = `${nextConfig.assetPrefix || ''}/_next/`;
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
