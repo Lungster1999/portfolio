cb10c100e3bd6347dc0a38efcc32796ac0f313c2
Lungile <lungile.matsinhe@gmail.com>
2023-06-26 18:04:20 +0200

    remove image

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import { motion } from "framer-motion";
 
@@ -19,7 +18,7 @@ const Skill = ({ directionLeft }: Props) => {
             className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-500 ease-in-out'
         />
 
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-500 ease-in-out group-hover:rounded-full group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>100%</p>
             </div>
--- next.config.js
+++ next.config.js
@@ -1,35 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Set the assetPrefix to the repository name
-    // Replace 'repository-name' with your actual repository name
-    // Make sure to include the trailing slash '/'
-    assetPrefix: '/portfolio/',
-  
-    // Specify basePath if your application is not hosted at the root of the domain
-    // For example, if your GitHub Pages URL is https://username.github.io/my-app/
-    // Set basePath to '/my-app'
-  
-    // Configure the webpack publicPath to match the assetPrefix
-    // This is required for correct asset loading on GitHub Pages
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.output.publicPath = `${nextConfig.assetPrefix || ''}/_next/`;
-      return config;
-    },
+const nextConfig = {}
 
-// Configure the images feature
-experimental: {
-    images: {
-      domains: ['https://static.wixstatic.com',
-      'https://upload.wikimedia.org',
-      'https://www.ctelecoms.com.sa',
-      'https://www.freecodecamp.org',
-      'https://root-it.uk',
-      'data:image',
-      'https://lh3.googleusercontent.com'], // Add the domains from which you will load images
-    },
-  },
-};
-
-  
-  module.exports = nextConfig;
+module.exports = nextConfig
   
